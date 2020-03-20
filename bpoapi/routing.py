b0da47b2eaa27a8d1938c093f429272a08262339
Armstrong <armstrongsouljah@gmail.com>
2020-03-20 07:47:52 +0300

    remove unnecessary code

--- bpoapi/routing.py
+++ bpoapi/routing.py
@@ -1,16 +1,12 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from chat.consumers import (
-    ChatMessageConsumer,
-)
 from chatterbox.consumers import ChatterboxConsumer
 
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
         URLRouter([
-         path('<username>/chat', ChatMessageConsumer),
          path('messages/<username>/send', ChatterboxConsumer)
         ])
     )
